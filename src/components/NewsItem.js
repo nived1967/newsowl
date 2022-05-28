32471ca8431d8771699c644c71910955c4e3e4b8
nived1967 <81697680+nived1967@users.noreply.github.com>
2022-05-29 00:32:28 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date,source } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+                {source}
+              </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZIJUOnS_he8z8fgK_BG7f5-Kal8yE_4KP8g&usqp=CAU"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}...-{source}
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more...
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
